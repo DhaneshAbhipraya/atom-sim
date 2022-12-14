76470c843524eeed8c67db14a4f995b52476e220
DhaneshAbhipraya <dhanesh.abhipraya@gmail.com>
2022-12-14 11:05:39 +0700

    remove unused imports and add constraint strength

--- constraint.py
+++ constraint.py
@@ -1,13 +1,12 @@
-from math import atan2
 from atom import Atom
-from typing import Any
 from pygame import Vector2
 
 
 class Constraint:
-    def __init__(self, atom1: Atom, atom2: Atom) -> None:
+    def __init__(self, atom1: Atom, atom2: Atom, factor: float = 1) -> None:
         self.atom1 = atom1
         self.atom2 = atom2
+        self.factor = factor
 
     def update(self):
         atom1angle = self.atom1.pos.angle_to(self.atom2.pos)
@@ -15,8 +14,8 @@ class Constraint:
         x = Vector2()
         x.from_polar((1, atom1angle))
         x = x + (self.atom2.pos - self.atom1.pos)
-        self.atom1.apply_force(x)  # type: ignore
+        self.atom1.apply_force(x * self.factor)  # type: ignore
         y = Vector2()
         y.from_polar((1, atom2angle))
         y = y + (self.atom1.pos - self.atom2.pos)
-        self.atom2.apply_force(y)  # type: ignore
+        self.atom2.apply_force(y * self.factor)  # type: ignore
